518dcabb6035ef910b1758f5a7f7789fd553dae5
Laura Montes <laumoen@gmail.com>
2025-08-11 10:47:12 +0000

    user

--- src/front/pages/Perfil.jsx
+++ src/front/pages/Perfil.jsx
@@ -3,15 +3,22 @@ import style from "../pages/Perfil.module.css"
 export const Perfil = () => {
     return (
         <div className={style.container}>
-            <div className={`${style.left} rounded-5 p-5`}>
-                <p>Foto</p>
-                <p>Nombre y apellidos</p>
-                <p>Correo electrónico</p>
+            <div className={`${style.left} rounded-5`}>
+                <div className={`${style.leftContent} my-auto`}>
+                    <div className={`${style.userPhoto} rounded-5`}>
+                        <p>Foto</p>
+                    </div>
+                    <p>Nombre y apellidos</p>
+                    <p>Correo electrónico</p>
+                    <p>Cambia tu contraseña</p>
+                </div>
             </div>
-            <div className={`${style.right} rounded-5 p-5`}>
-                <p>Respuestas registradas de la aplicación</p>
-                <p>Sección para puntuar respuestas</p>
-                <p>Enlace a tus redes sociales</p>
+            <div className={`${style.right} rounded-5`}>
+                <div className={`${style.rightContent} my-auto`}>
+                    <p>Respuestas registradas de la aplicación</p>
+                    <p>Sección para puntuar respuestas</p>
+                    <p>Enlace a tus redes sociales</p>
+                </div>
             </div>
         </div>
     );
