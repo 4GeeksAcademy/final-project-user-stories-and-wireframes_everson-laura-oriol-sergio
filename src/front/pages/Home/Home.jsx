1706e2b7e018cba11f12f3df213910122f7ddcba
Oriol383 <oriolcm02@gmail.com>
2025-08-22 11:42:35 +0000

    Cambios-Masonry-Perfil-Stickers

--- src/front/pages/Home/Home.jsx
+++ src/front/pages/Home/Home.jsx
@@ -1,40 +1,123 @@
 import React, { useEffect } from "react";
-import useGlobalReducer from "../../hooks/useGlobalReducer.jsx";
 import { Container } from "react-bootstrap";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const responsive = {
 	superLargeDesktop: {
 		breakpoint: { max: 4000, min: 3000 },
-		items: 1
+		items: 1,
 	},
 	desktop: {
 		breakpoint: { max: 3000, min: 1024 },
-		items: 1
+		items: 1,
 	},
 	tablet: {
 		breakpoint: { max: 1024, min: 464 },
-		items: 1
+		items: 1,
 	},
 	mobile: {
 		breakpoint: { max: 464, min: 0 },
-		items: 1
-	}
+		items: 1,
+	},
 };
 
 export const Home = () => {
+	useEffect(() => {
+		const stickers = document.querySelectorAll(".sticker");
+
+		stickers.forEach((sticker) => {
+			let offsetX, offsetY;
+
+			// Mover stickers en PC
+			const mouseDownHandler = (e) => {
+				offsetX = e.clientX - sticker.getBoundingClientRect().left;
+				offsetY = e.clientY - sticker.getBoundingClientRect().top;
+
+				document.addEventListener("mousemove", mouseMoveHandler);
+				document.addEventListener("mouseup", mouseUpHandler);
+			};
+
+			const mouseMoveHandler = (e) => {
+				sticker.style.left = `${e.clientX - offsetX}px`;
+				sticker.style.top = `${e.clientY - offsetY}px`;
+			};
+
+			const mouseUpHandler = () => {
+				document.removeEventListener("mousemove", mouseMoveHandler);
+				document.removeEventListener("mouseup", mouseUpHandler);
+			};
+
+			//  Mover stickers en movil
+			const touchStartHandler = (e) => {
+				const touch = e.touches[0];
+				offsetX = touch.clientX - sticker.getBoundingClientRect().left;
+				offsetY = touch.clientY - sticker.getBoundingClientRect().top;
+
+				document.addEventListener("touchmove", touchMoveHandler);
+				document.addEventListener("touchend", touchEndHandler);
+			};
+
+			const touchMoveHandler = (e) => {
+				const touch = e.touches[0];
+				sticker.style.left = `${touch.clientX - offsetX}px`;
+				sticker.style.top = `${touch.clientY - offsetY}px`;
+			};
+
+			const touchEndHandler = () => {
+				document.removeEventListener("touchmove", touchMoveHandler);
+				document.removeEventListener("touchend", touchEndHandler);
+			};
+
+
+			sticker.addEventListener("mousedown", mouseDownHandler);
+			sticker.addEventListener("touchstart", touchStartHandler, { passive: false });
+		});
+
+
+		return () => {
+			stickers.forEach((sticker) => {
+				sticker.replaceWith(sticker.cloneNode(true));
+			});
+		};
+	}, []);
 
 	return (
-
 		<div className="justify-content-center align-items-center text-center main">
-			{/*<div class="sticker-container">
-				<img src="src/front/assets/img/pegatina1.png" class="sticker" draggable="false" id="sticker1" />
-				<img src="src/front/assets/img/pegatina2.png" class="sticker" draggable="false" id="sticker2" />
-				<img src="src/front/assets/img/pegatina3.png" class="sticker" draggable="false" id="sticker3" />
-				<img src="src/front/assets/img/pegatina4.png" class="sticker" draggable="false" id="sticker4" />
-				<img src="src/front/assets/img/pegatina5.png" class="sticker" draggable="false" id="sticker5" />
-			</div>*/}
+			{ }
+			<div className="sticker-container">
+				<img
+					src="src/front/assets/img/pegatina1.png"
+					className="sticker"
+					draggable="false"
+					id="sticker1"
+				/>
+				<img
+					src="src/front/assets/img/pegatina2.png"
+					className="sticker"
+					draggable="false"
+					id="sticker2"
+				/>
+				<img
+					src="src/front/assets/img/pegatina3.png"
+					className="sticker"
+					draggable="false"
+					id="sticker3"
+				/>
+				<img
+					src="src/front/assets/img/pegatina4.png"
+					className="sticker"
+					draggable="false"
+					id="sticker4"
+				/>
+				<img
+					src="src/front/assets/img/pegatina5.png"
+					className="sticker"
+					draggable="false"
+					id="sticker5"
+				/>
+			</div>
+
 			<div className="wave-container d-flex w-50 m-auto pt-5">
 				<p className="wave-text slogan text-center">
 					<span>S</span>
@@ -53,11 +136,14 @@ export const Home = () => {
 					<span>s</span>
 				</p>
 			</div>
+
 			<div className="slogan-2 text-center mb-5">Desliza, descubre</div>
 			<div className="text-center mx-auto start">¿Empezamos?</div>
 			<div className="text-center mx-auto mt-2 try">¡Pruébame!</div>
 			<div className="d-flex text-center mx-auto justify-content-center pt-4">
-				<div className="text-center"><i className="fa-solid fa-angle-down"></i></div>
+				<div className="text-center">
+					<i className="fa-solid fa-angle-down"></i>
+				</div>
 			</div>
 
 			<Carousel
@@ -75,19 +161,34 @@ export const Home = () => {
 				containerClass="carousel-container"
 				removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
 				itemClass="carousel-item-padding-40-px"
-				className="mt-2 mb-5">
-				<div className="quote large-quote">Estás a un solo swipe de decir adiós al aburrimiento.</div>
-				<div className="quote large-quote">Descubre géneros de los que jamás pensaste que formarías parte.</div>
-				<div className="quote large-quote">Realiza formularios deslizables y almacena tus respuestas.</div>
-				<div className="quote large-quote">Descubre qué ver, qué leer, qué vivir.</div>
-				<div className="quote large-quote">Recomendaciones que te enganchan y te cambian.</div>
+				className="mt-2 mb-5"
+			>
+				<div className="quote large-quote">
+					Estás a un solo swipe de decir adiós al aburrimiento.
+				</div>
+				<div className="quote large-quote">
+					Descubre géneros de los que jamás pensaste que formarías parte.
+				</div>
+				<div className="quote large-quote">
+					Realiza formularios deslizables y almacena tus respuestas.
+				</div>
+				<div className="quote large-quote">
+					Descubre qué ver, qué leer, qué vivir.
+				</div>
+				<div className="quote large-quote">
+					Recomendaciones que te enganchan y te cambian.
+				</div>
 			</Carousel>
 
 			<div className="d-flex text-center mx-auto justify-content-center pb-4">
-				<div className="text-center"><i className="fa-solid fa-angle-up"></i></div>
+				<div className="text-center">
+					<i className="fa-solid fa-angle-up"></i>
+				</div>
 			</div>
 
-			<div className="text-center slogan-3">Deslízame y descubrirás todo lo que te ofrecemos.</div>
+			<div className="text-center slogan-3">
+				Deslízame y descubrirás todo lo que te ofrecemos.
+			</div>
 		</div>
 	);
 };
--- src/front/pages/Perfil.jsx
+++ src/front/pages/Perfil.jsx
@@ -1,25 +1,21 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import style from "../pages/Perfil.module.css";
 import { Container, Row } from "react-bootstrap";
-
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 export const Perfil = () => {
     const lineRef = useRef(null);
     const containerRef = useRef(null);
     const userLocalStorage = localStorage.getItem("user")
     const [user, setUser] = useState(null)
-
     useEffect(() => {
         if (userLocalStorage) {
             setUser(JSON.parse(userLocalStorage))
         }
     }, [])
-
-
     useEffect(() => {
         const container = containerRef.current;
         const links = container.querySelectorAll("a");
         const line = lineRef.current;
-
         function handleHover(e) {
             const rect = e.target.getBoundingClientRect();
             const containerRect = container.getBoundingClientRect();
@@ -30,23 +26,18 @@ export const Perfil = () => {
             line.style.opacity = "1";
             line.classList.add(style.animateBackground);
         }
-
         function handleLeave() {
             line.style.opacity = "0";
             line.classList.remove(style.animateBackground);
         }
-
         function stopZigzagAnimation() {
             line.classList.remove(style.animateBackground);
         }
-
         links.forEach((link) => {
             link.addEventListener("mouseenter", handleHover);
             link.addEventListener("mousemove", stopZigzagAnimation);
         });
-
         container.addEventListener("mouseleave", handleLeave);
-
         return () => {
             links.forEach((link) => {
                 link.removeEventListener("mouseenter", handleHover);
@@ -55,7 +46,6 @@ export const Perfil = () => {
             container.removeEventListener("mouseleave", handleLeave);
         };
     }, []);
-
     return (
         <Container className={style.container}>
             <Row className={`${style.row} position-relative mt-4`} ref={containerRef}>
@@ -66,21 +56,19 @@ export const Perfil = () => {
                     <li className={`${style.links} nav-item`} role="presentation">
                         <a href="#" className={`${style.link} nav-link`} id="historial" role="tab" aria-controls="pills-historial" aria-selected="false" data-bs-toggle="pill" data-bs-target="#pills-historial"> Mi historial </a>
                     </li>
-                    <div ref={lineRef} className={style.zigzagLine}></div>
                 </ul>
             </Row>
             <div className="tab-content" id="pills-tabContent">
-
-             {
+                {
                     !user ? (
                         <span>Loading</span>
-                    ) : (  
+                    ) : (
                         <div className={`${style.perfil} tab-pane fade show active`} id="pills-perfil" role="tabpanel" aria-labelledby="perfil">
                             <div className={`${style.profile} rounded-5 w-50 mx-auto mt-5 d-flex justify-content-center`}>
                                 <div className={`${style.info}`}>
                                     <label>Nombre</label>
                                     <div><input type="text" className="form-control text-center w-100" placeholder="Nombre" value={user.name} /></div>
-                                    <br />                    
+                                    <br />
                                     <br />
                                     <label>Username</label>
                                     <div><input type="text" className="form-control text-center w-100" placeholder="Username" value={user.username} /></div>
@@ -105,7 +93,19 @@ export const Perfil = () => {
                     )
                 }
                 <div className={`${style.historial} tab-pane fade`} id="pills-historial" role="tabpanel" aria-labelledby="historial">
-                    x
+                    <ResponsiveMasonry
+                        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+                        gutterBreakPoints={{ 350: "12px", 750: "16px", 900: "24px" }}
+                    >
+                        <Masonry>
+                            <div className={style.boxtesthori}>1</div>
+                            <div className={style.boxtesthori1}>2</div>
+                            <div className={style.boxtesthori2}>3</div>
+                            <div className={style.boxtesthori3}>4</div>
+                            <div className={style.boxtesthori4}>5</div>
+                            <div className={style.boxtesthori5}>6</div>
+                        </Masonry>
+                    </ResponsiveMasonry>
                 </div>
             </div>
         </Container>
