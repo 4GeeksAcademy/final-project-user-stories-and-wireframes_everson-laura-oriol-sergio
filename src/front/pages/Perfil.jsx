e1e3cfdc89a96a36f075bb825f9220681b8b8f99
Laura Montes <laumoen@gmail.com>
2025-08-23 16:44:35 +0000

    historial acabado

--- src/front/pages/Perfil.jsx
+++ src/front/pages/Perfil.jsx
@@ -2,6 +2,7 @@ import { useRef, useEffect, useState } from "react";
 import style from "../pages/Perfil.module.css";
 import { Container, Row } from "react-bootstrap";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import { SiStryker } from "react-icons/si";
 
 export const Perfil = () => {
     const lineRef = useRef(null);
@@ -57,7 +58,7 @@ export const Perfil = () => {
 
     return (
         <Container className={style.container}>
-            <Row className={`${style.row} position-relative mt-4`} ref={containerRef}>
+            <Row className={`${style.row} position-relative mt-4 mb-5`} ref={containerRef}>
                 <ul className={`${style.navs} nav nav-link d-flex mx-auto`} id="pills-tab" role="tablist">
                     <li className={`${style.links} nav-item`} role="presentation">
                         <a href="#" className={`${style.link} nav-link active`} id="perfil" role="tab" aria-controls="pills-perfil" aria-selected="true" data-bs-toggle="pill" data-bs-target="#pills-perfil"> Mi perfil </a>
@@ -94,16 +95,16 @@ export const Perfil = () => {
                         gutterBreakPoints={{ 350: "12px", 750: "16px", 900: "24px" }}
                     >
                         <Masonry>
-                            <div className={style.boxtesthori}>1</div>
-                            <div className={style.boxtesthori1}>2</div>
-                            <div className={style.boxtesthori2}>3</div>
-                            <div className={style.boxtesthori3}>4</div>
-                            <div className={style.boxtesthori4}>5</div>
-                            <div className={style.boxtesthori5}>6</div>
+                            <div className={`${style.boxtest} rounded-5`}></div>
+                            <div className={`${style.boxtest1} rounded-5`}></div>
+                            <div className={`${style.boxtest2} rounded-5`}></div>
+                            <div className={`${style.boxtest3} rounded-5`}></div>
+                            <div className={`${style.boxtest4} rounded-5`}></div>
+                            <div className={`${style.boxtest5} rounded-5`}></div>
                         </Masonry>
                     </ResponsiveMasonry>
                 </div>
             </div>
         </Container>
     );
-};
+}
